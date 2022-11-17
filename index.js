980f6f8897459b7a9225eaa1d2dadb630f79b69a
JasonChiu2 <stemlads@gmail.com>
2022-11-17 10:13:56 -0500

    bruh

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const DOMSelectors = {
   artist: document.getElementsByClassName("artist"),
   album: document.getElementsByClassName("album"),
   button: document.getElementById("btn"),
+  name: document.getElementsByClassName("name"),
+  form: document.querySelector("#form"),
 };
 
-function CreateCard() {}
+DOMSelectors.form.addEventListener("submit", function (Event) {
+  Event.preventDefault(refresh);
+  let artist = DOMSelectors.artist.value;
+  let album = DOMSelectors.album.value;
+  let name = DOMSelectors.name.value;
+  console.log(Event);
+});
